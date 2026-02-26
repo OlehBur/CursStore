54f0227ec0f9a8c53f0fa0363084de1abcd423ff
OlehBur <burdeyniy.007.com@gmail.com>
2026-02-26 16:18:33 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo react" alt="React logo"  />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo" alt="Vite logo"/>
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>BURRR</code> and save to test BUUUURRRR
         </p>
       </div>
       <p className="read-the-docs">
